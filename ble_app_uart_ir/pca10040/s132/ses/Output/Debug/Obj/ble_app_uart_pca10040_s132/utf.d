041d470c9653c19ba8a8d1a7e67b9cfb4691ec19
Arturas Stanislovavicius <arrrtas@gmail.com>
2022-05-23 14:08:38 +0200

    the latest version of the code

---
+++ ble_app_uart_ir/pca10040/s132/ses/Output/Debug/Obj/ble_app_uart_pca10040_s132/utf.d
@@ -0,0 +1,6 @@
+Output/Debug/Obj/ble_app_uart_pca10040_s132/utf.o: \
+ C:\Users\arrrt\source\repos\nrf_sdk\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.c \
+ C:\Users\arrrt\source\repos\nrf_sdk\nRF5_SDK_17.1.0_ddde560\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.68/include/stdint.h
---
+++ ble_app_uart_ir/pca10040/s132/ses/Output/Release/Obj/ble_app_uart_pca10040_s132/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_app_uart_pca10040_s132/ses_startup_nrf_common.o: \
+ C:\Users\arrrt\source\repos\nrf_sdk\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
